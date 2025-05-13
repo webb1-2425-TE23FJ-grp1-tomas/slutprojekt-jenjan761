be3d3259d5d0ce6a5f5e62a44e3d2ac88a79054b
jenjan761 <jenjan761@edu.linkoping.se>
2025-05-13 08:56:54 +0000

    wefewerg

--- Javaskript/main.js
+++ Javaskript/main.js
@@ -1,6 +1,18 @@
 
 const openmenubtn = document.querySelector("#menu-open");
+const closemenubtn = document.querySelector("#menu-close");
 const menu = document.querySelector(".menu");
 
-menu.style.display = "flex";
-console.log(menu);
+function openMenu(event){
+    console.log("open on click");
+    menu.style.display = "flex";
+    openmenubtn.style.display = "none";
+}
+openmenubtn.addEventListener("click", openMenu);
+
+function closeMenu(event){
+    console.log("close on click");
+    menu.style.display = "none";
+    openmenubtn.style.display = "block";
+}
+closemenubtn.addEventListener("click", closeMenu);
